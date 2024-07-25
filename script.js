1abc2806699e24f65475f943f688a41d4fbab77b
陳恩懂 <austinhandsome@chenendongdeMacBook-Air.local>
2024-07-25 08:58:40 +0800

    2 games

---
+++ script.js
@@ -0,0 +1,16 @@
+const button = document.getElementById("PLAYbutton");
+
+const color = [
+    {name: "red", value: '#FF6B6B'},
+    {name: "blue", value: '#4abbf8'},
+    {name: "yellow", value: '#ffe100'},
+    {name: "green", value: '#4fbd2d'},
+    {name: "purple", value: '#942dbd'}
+];
+
+function changeColor() {
+    const random = color[Math.floor(Math.random()*5)];
+    document.body.style.backgroundColor = random.value;
+};
+
+button.addEventListener("click", changeColor);
